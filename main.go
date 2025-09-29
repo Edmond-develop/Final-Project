54d80e83f2387df86963e0557982fd1053871828
Edmond-develop <kepler6j@gmail.com>
2025-09-29 17:15:30 +0300

    Second commit. Add db in project

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"go1f/pkg/db"
 	"go1f/pkg/server"
+	"os"
 )
 
 func main() {
-	server.Run()
+	dbFile := os.Getenv("TODO_DBFILE")
+	if dbFile == "" {
+		dbFile = "scheduler.db"
+	}
+	err := db.Init(dbFile)
+	if err != nil {
+		fmt.Println("init scheduler error:", err)
+		return
+	}
+	defer db.DB.Close()
+	err = server.Run()
+	if err != nil {
+		fmt.Println("server run error:", err)
+		return
+	}
 }
